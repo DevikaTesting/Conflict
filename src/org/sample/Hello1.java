d2811e0123f9dfc2c4b011b4d274225f7452f823
DevikaTesting <k.devi.divi@gmail.com>
2019-03-21 06:45:51 +0530

    created

--- src/org/sample/Hello1.java
+++ src/org/sample/Hello1.java
@@ -1,8 +1,12 @@
 package org.sample;
 
 public class Hello1 {
+	public void add() {
+		System.out.println("a");
+	}
 	public static void main(String[] args) {
 		System.out.println("base");
 	}
+
 
 }
