a101fa30384babcd9c053e214aff4d826ea4f231
DevikaTesting <k.devi.divi@gmail.com>
2019-03-21 06:47:34 +0530

    created

--- src/org/sample/Hello1.java
+++ src/org/sample/Hello1.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class Hello1 {
+	public void WebTable() {
+		System.out.println("webtable");
+	}
 	public static void main(String[] args) {
 		System.out.println("base");
 	}
