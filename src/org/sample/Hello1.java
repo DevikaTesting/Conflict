e8fc429ffbe3a0f6aee1f75a15b85e731f69e17f
DevikaTesting <k.devi.divi@gmail.com>
2019-03-21 06:52:53 +0530

    resolved

--- src/org/sample/Hello1.java
+++ src/org/sample/Hello1.java
@@ -4,8 +4,13 @@ public class Hello1 {
 	public void WebTable() {
 		System.out.println("webtable");
 	}
+	public void add() {
+		System.out.println("a");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("base");
 	}
+
 
 }
